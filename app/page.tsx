2b81192234a5b666143aa7ed63a7cc4f20d19001
Fiza <fizaramzan4@gmail.com>
2026-04-08 19:43:38 +0400

    fix: dynamic rendering

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Link from 'next/link';
 import { prisma } from '@/lib/prisma';
 import PlaidLink from '@/components/PlaidLink';
 
+
+export const dynamic = 'force-dynamic';
 export default async function Home() {
   // 1. Check if ANY account exists in the database
   const existingAccount = await prisma.account.findFirst();
